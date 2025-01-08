7ea5d40fe4539f4e7b21c2ee438648d0d3c3ff85
Rjpererag <rjperera98@gmail.com>
2025-01-08 13:27:18 +0100

    source code

---
+++ src/dataclasses/bucket.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Bucket:
+    name: str = None
+    creation_date: datetime = datetime.now()
+
+@dataclass
+class Content:
+    name: str = None
+    last_modified: datetime = datetime.now()
---
+++ src/parsers/bucket.py
@@ -0,0 +1,18 @@
+from ..dataclasses.bucket import *
+
+def parse_bucket(buckets: list[dict]) -> list[Bucket]:
+    return [
+        Bucket(
+            name=bucket.get("Name"),
+            creation_date=bucket.get("CreationDate"),     
+        ) for bucket in buckets
+    ]
+
+def parse_contents(contents_raw: list[dict]) -> list[Content]:
+    return [
+        Content(
+            name=content.get("Key"),
+            last_modified=content.get("LastModified")
+        )
+        for content in contents_raw
+    ]
